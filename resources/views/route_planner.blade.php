d90bd3f248a599a5c1ac3854696916ea51e544d8
MikeLG90 <miguelgomez29117@hotmail.com>
2025-12-08 15:50:51 -0500

    fix rp

--- resources/views/route_planner.blade.php
+++ resources/views/route_planner.blade.php
@@ -220,7 +220,7 @@
             }
 
             try {
-                const response = await fetch('http://127.0.0.1/api/rutas/store', {
+                const response = await fetch('https://rutas-up-backend.onrender.com/api/rutas/store', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
